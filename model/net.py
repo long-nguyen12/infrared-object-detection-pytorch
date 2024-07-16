5f0c717707432d6b9cf55bf5e67c59929c1b28e2
Nguyen Long <longnguyenhoang.ict@gmail.com>
2024-07-16 10:42:30 +0700

    add cross attention

--- model/net.py
+++ model/net.py
@@ -7,95 +7,27 @@ from .modules.cbam import CBAM
 from model.modules.cfp import CFPModule
 from timm.layers import trunc_normal_
 import math
+from model.modules.attentions import AA_kernel
 
 
-class CFR(nn.Module):
-    def __init__(self, in_ch, inc_ch):
-        super(CFR, self).__init__()
-        self.conv0 = nn.Conv2d(in_ch, inc_ch, 3, padding=1)
-        self.conv1 = nn.Conv2d(inc_ch, 64, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(2, 2, ceil_mode=True)
+class CrossAttention(nn.Module):
+    def __init__(self, lower_c, higher_c) -> None:
+        super().__init__()
 
-        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(2, 2, ceil_mode=True)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.pool3 = nn.MaxPool2d(2, 2, ceil_mode=True)
+        self.aa_1 = AA_kernel(lower_c, lower_c)
+        self.aa_2 = AA_kernel(higher_c, lower_c)
+        self.up = nn.Upsample(size=2, mode="bilinear")
 
-        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.pool4 = nn.MaxPool2d(2, 2, ceil_mode=True)
+    def forward(self, x1, x2):
+        att = self.avg_pool(x1)
 
-        #####
+        x1 = self.aa_1(x1)
+        x2 = self.aa_2(x2)
 
-        self.conv5 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn5 = nn.BatchNorm2d(64)
-        self.relu5 = nn.ReLU(inplace=True)
-
-        #####
-
-        self.conv_d4 = nn.Conv2d(128, 64, 3, padding=1)
-        self.bn_d4 = nn.BatchNorm2d(64)
-        self.relu_d4 = nn.ReLU(inplace=True)
-
-        self.conv_d3 = nn.Conv2d(128, 64, 3, padding=1)
-        self.bn_d3 = nn.BatchNorm2d(64)
-        self.relu_d3 = nn.ReLU(inplace=True)
-
-        self.conv_d2 = nn.Conv2d(128, 64, 3, padding=1)
-        self.bn_d2 = nn.BatchNorm2d(64)
-        self.relu_d2 = nn.ReLU(inplace=True)
-
-        self.conv_d1 = nn.Conv2d(128, 64, 3, padding=1)
-        self.bn_d1 = nn.BatchNorm2d(64)
-        self.relu_d1 = nn.ReLU(inplace=True)
-
-        self.conv_d0 = nn.Conv2d(64, 1, 3, padding=1)
-
-        self.upscore2 = nn.Upsample(scale_factor=2, mode="bilinear")
-
-    def forward(self, x):
-
-        hx = x
-        hx = self.conv0(hx)
-
-        hx1 = self.relu1(self.bn1(self.conv1(hx)))
-        hx = self.pool1(hx1)
-
-        hx2 = self.relu2(self.bn2(self.conv2(hx)))
-        hx = self.pool2(hx2)
-
-        hx3 = self.relu3(self.bn3(self.conv3(hx)))
-        hx = self.pool3(hx3)
-
-        hx4 = self.relu4(self.bn4(self.conv4(hx)))
-        hx = self.pool4(hx4)
-
-        hx5 = self.relu5(self.bn5(self.conv5(hx)))
-
-        hx = self.upscore2(hx5)
-
-        d4 = self.relu_d4(self.bn_d4(self.conv_d4(torch.cat((hx, hx4), 1))))
-        hx = self.upscore2(d4)
-
-        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx, hx3), 1))))
-        hx = self.upscore2(d3)
-
-        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx, hx2), 1))))
-        hx = self.upscore2(d2)
-
-        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx, hx1), 1))))
-
-        residual = self.conv_d0(d1)
-
-        return x + residual
+        out = x1 + self.up(x2)
+        return att * out
 
 
 class SegmentNet(nn.Module):
@@ -105,6 +37,10 @@ class SegmentNet(nn.Module):
         self.backbone = res2net50_v1b(pretrained=False)
         self.params = self.backbone.channels
 
+        self.cross_1 = CrossAttention(64, 128)
+        self.cross_2 = CrossAttention(128, 256)
+        self.cross_3 = CrossAttention(256, 512)
+
         self.decoder = UPerHead(self.params, 128, 1)
 
         self.output_0 = nn.Conv2d(128, 1, 1)
@@ -112,13 +48,15 @@ class SegmentNet(nn.Module):
         self.output_2 = nn.Conv2d(128, 1, 1)
         self.output_3 = nn.Conv2d(128, 1, 1)
 
-        self.cfr = CFR(1, 64)
-
         self.final = nn.Conv2d(5, 1, 3, 1, 1)
 
     def forward(self, x, warm_flag):
         enc_out = self.backbone(x)
         x0, x1, x2, x3 = enc_out
+
+        x0 = self.cross_1(x0, x1)
+        x1 = self.cross_1(x1, x2)
+        x2 = self.cross_1(x2, x3)
 
         sub_masks, global_mask = self.decoder([x0, x1, x2, x3])
 
